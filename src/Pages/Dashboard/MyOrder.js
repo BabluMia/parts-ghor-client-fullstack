ab5b709da4153dba784bbe9a1e6a2776119d0f53
Bablu Mia <bablumiah2000@gmail.com>
2022-05-26 10:53:06 +0600

    redmi added

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -4,6 +4,7 @@ import { useQuery } from "react-query";
 import swal from "sweetalert";
 import auth from "../../firebase.init";
 import Loading from "../Shared/Loading";
+import OrderDel from "./OrderDel";
 
 const MyOrder = () => {
   const [user] = useAuthState(auth);
@@ -18,6 +19,8 @@ const MyOrder = () => {
   })
     .then((res) => res.json())
     .then((data) => setMyOrder(data));
+
+  const [order,setOrder] = useState(null)
 
   return (
     <div>
@@ -40,14 +43,20 @@ const MyOrder = () => {
                 <td>{order?.orderQuantity}</td>
                 <td>{order?.totalAmount}</td>
                 <td>
-                  <button class="btn btn-sm">Pay</button>
-                  <button class="btn btn-sm mx-2">Cancel</button>
+                  <button class="btn btn-sm mx-2">Pay</button>
+                  <a href="#my-modal-2" onClick={()=>setOrder(order)} class="btn btn-sm">
+                    Cancel
+                  </a>
                 </td>
               </tr>
             ))}
-            {console.log(myOrder)}
+            
           </tbody>
         </table>
+        {/* --------- */}
+        {
+          order !== null && <OrderDel setOrder={setOrder } order={order} />
+        }
       </div>
     </div>
   );
---
+++ src/Pages/Dashboard/OrderDel.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import swal from 'sweetalert';
+
+const OrderDel = ({order,setOrder}) => {
+    // delete
+  const handleDelete = (id) => {
+    const url = `http://localhost:5000/orders/${id}`;
+
+    fetch(url, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          swal({
+            title: "Delete Notification",
+            text: "Deleted SuccessFully",
+            icon: "success",
+          });
+        }
+        setOrder(null)
+      });
+  };
+    return (
+        
+
+        <div class="modal" id="my-modal-2">
+          <div class="modal-box">
+          <label onClick={()=>setOrder(null)} for="my-modal-2" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+
+            <h3 class="font-bold text-lg">
+             { `Are you sure u want to delete ${order.itemName} order` }
+            </h3>
+            <p class="py-4">
+             If you delete you do not able to see it on your order .!!
+            </p>
+            <div class="modal-action">
+              <button class="btn btn-sm mx-2" onClick={() => handleDelete(order._id)}>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+        
+    );
+};
+
+export default OrderDel;
